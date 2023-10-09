c366901cb4d6605223a7fda984126949c9e33fdc
Taufeeq Ahmed <taufeeq.ahmed@cogoport.com>
2023-10-09 16:23:07 +0530

    european ports links are added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ SUPPORTED_AIPORT_TYPES = ['midsized airport','large airport']
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import json
 driver = webdriver.Chrome()
 
@@ -14,37 +15,47 @@ euroCountriesLength = len(driver.find_elements(By.CSS_SELECTOR, selector))
 EUROPE_LINKS = []
 
 
-for i in range(0, euroCountriesLength):
-    country = driver.find_elements(By.CSS_SELECTOR,selector)[i]
+for p in range(0, euroCountriesLength):
+    country = driver.find_elements(By.CSS_SELECTOR,selector)[p]
     country.click()
+    print("p is :",str(p))
     
-    airPortsList = driver.find_elements(By.CSS_SELECTOR,'#w1 > table > tbody > tr')
-    
-    for i in range(0,len(airPortsList)):
+    is_pagination_next = True
+
+    while(is_pagination_next):
+            airPortsList = driver.find_elements(By.CSS_SELECTOR,'#w1 > table > tbody > tr')
+            for q in range(0,len(airPortsList)):
+                airPortsList = driver.find_elements(By.CSS_SELECTOR,'#w1 > table > tbody > tr')
+                print("length :"+str(len(airPortsList)))
+                print("q is :"+str(q))
+                row = airPortsList[q]
+                cells = row.find_elements(By.CSS_SELECTOR,'*')
         
-        is_pagination_next = True
+                airport_type = cells[-1].text.lower()
+                airport_link = cells[1].find_element(By.TAG_NAME,'a').get_attribute('href')
         
-        while(is_pagination_next):
-            row = airPortsList[i]
-            cells = row.find_elements(By.CSS_SELECTOR,'*')
-        
-            airport_type = cells[-1].text.lower()
-            airport_link = cells[1].find_element(By.TAG_NAME,'a').get_attribute('href')
-        
-            if airport_type in SUPPORTED_AIPORT_TYPES:
-                EUROPE_LINKS.append(airport_link)
+                if airport_type in SUPPORTED_AIPORT_TYPES:
+                    EUROPE_LINKS.append(airport_link)
     
             try:
-                driver.find_element(By.CSS_SELECTOR,'#w1 > ul > li.next')
+                driver.find_element(By.CSS_SELECTOR,'#w1 > ul')
+                
+                try:
+                    driver.find_element(By.CSS_SELECTOR,'#w1 > ul > li.next.page-item.disabled')
+                    is_pagination_next=False
+                except NoSuchElementException:
+                    is_pagination_next=True
+                    
             except NoSuchElementException:
-                is_pagination_next=False
+                    is_pagination_next=False
             
             if is_pagination_next:
+                print("next_button clicked")
                 next_button = driver.find_element(By.CSS_SELECTOR,'#w1 > ul > li.next')
                 next_button.click()
+                
+    driver.get("https://metar-taf.com/countries")
 
-    driver.back()
-    
     
 json_data = json.dumps(EUROPE_LINKS)
 with open("europe_airport_links.json", "w") as outfile:
